55777f3e1cee8ac01e022484834dd430ecdeb895
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2020-11-30 09:54:29 +0100

    Added some test files

---
+++ src/model/IProjectManagementModel.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface IProjectManagementModel {
+    void doStuff();
+}
---
+++ src/model/ProjectManagementModelManager.java
@@ -0,0 +1,7 @@
+package model;
+
+public class ProjectManagementModelManager implements IProjectManagementModel {
+    public void doStuff() {
+        System.out.println("Doing stuff");
+    }
+}
