f71f38266e19b4bda40eac3c74213db3715d0176
stefaniatomuta <304173@via.dk>
2020-12-02 11:03:15 +0100

    Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import model.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        Date date = new Date(1, 1, 1999);
+        System.out.println(date);
+
     }
 }
