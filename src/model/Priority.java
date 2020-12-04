0c3b9925c4373430eb8dd58df6445c7d394a5640
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2020-12-04 12:42:28 +0100

    aefa

--- src/model/Priority.java
+++ src/model/Priority.java
@@ -4,5 +4,22 @@ public enum Priority
 {
   CRITICAL,
   HIGH,
-  LOW
+  LOW;
+
+  /**
+   * Method to get the string representation of the task's priority.
+   * @return The string representation of a priority in a proper format.
+   */
+  public String getName() {
+    switch (this) {
+      case CRITICAL:
+        return "Critical";
+      case HIGH:
+        return "High";
+      case LOW:
+        return "Low";
+      default:
+        throw new IllegalStateException("Encountered an illegal priority value");
+    }
+  }
 }
