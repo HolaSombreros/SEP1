fdc7034a6f56add62dd02a8515a959ca3236fc59
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2020-12-04 11:16:42 +0100

    rsgd

--- src/gui/DetailsAndEditRequirementController.java
+++ src/gui/DetailsAndEditRequirementController.java
@@ -3,6 +3,8 @@ package gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
@@ -11,21 +13,19 @@ import model.ProjectManagementModelManager;
 import model.RequirementStatus;
 import model.Type;
 
-import java.awt.*;
-
 public class DetailsAndEditRequirementController
-{/*
+{
   @FXML private Label idLabel;
   @FXML private Label relatedProjectLabel;
-  @FXML private TextField userStory;
+  @FXML private TextField userStoryInput;
   @FXML private DatePicker deadlineInput;
   @FXML private DatePicker startingDateInput;
   @FXML private TextField estimatedTimeInput;
   @FXML private ChoiceBox<Priority> priorityInput;
   @FXML private ChoiceBox<Type> typeInput;
   @FXML private ChoiceBox<RequirementStatus> statusInput;
-  @FXML private TextField hoursWorked;
-  @FXML private TextField responsibleTeamMember;
+  @FXML private TextField hoursWorkedInput;
+  @FXML private TextField responsibleTeamMemberInput;
   @FXML private TableView<TeamMemberListViewModel> teamMembersTable;
   @FXML private TableColumn<TeamMemberListViewModel, String> nameColumn;
   @FXML private TableColumn<TeamMemberListViewModel, String> idColumn;
@@ -34,5 +34,5 @@ public class DetailsAndEditRequirementController
   private ProjectManagementModelManager model;
   private ViewHandler viewHandler;
   private TeamMemberListViewModel viewModel;
-*/
+
 }
